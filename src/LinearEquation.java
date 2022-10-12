0f8f4e32bcecce832f3d1f4e92138cb8a48ab81f
BT_4N2_07 <ahelik4984@bths.edu>
2022-10-12 12:28:11 -0400

    finished LinearEquation and toString code

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -10,22 +10,22 @@ public class LinearEquation {
     private String slopeIntercept;
     private String distance;
     public LinearEquation(int x1, int y1, int x2, int y2){
-        firstPairLocation = "First pair: (" + x1 + "," + y1 + ")";
-        secondPairLocation = "Second pair: (" + x2 + "," + y2 + ")";
+        firstPairLocation = "(" + x1 + "," + y1 + ")";
+        secondPairLocation = "(" + x2 + "," + y2 + ")";
         double slope = (y2-y1)/(x2-x1);
-        slopeIs = "Slope of line: " + slope;
+        slopeIs = "" + slope;
         int yIntercept = (y1-((int)slope*x1));
-        yInterceptIs = "Y-intercept: " + yIntercept;
-        slopeIntercept = "y = " + (y2-y1) + "/" + (x2-x1) + "x" + "+ " + yIntercept;
-        distance = "Distance between points: " + Math.sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));
+        yInterceptIs = "" + yIntercept;
+        slopeIntercept = (y2-y1) + "/" + (x2-x1) + "x" + " + " + yIntercept;
+        distance = "" + Math.round((Math.sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1))))*100.0)/100.0;
     }
     public String toString(){
-        String toString = firstPairLocation + "/n";
-        toString = toString + secondPairLocation + "/n";
-        toString = toString + slopeIs + "/n";
-        toString = toString + yInterceptIs + "/n";
-        toString = toString + slopeIntercept + "/n";
-        toString = toString + distance + "/n";
+        String toString = "First pair: " + firstPairLocation + " \n";
+        toString = toString + "Second pair: " + secondPairLocation + " \n";
+        toString = toString + "Slope of line: " + slopeIs + " \n";
+        toString = toString + "Y-intercept: " + yInterceptIs + " \n";
+        toString = toString + "Slope intercept form: y = " + slopeIntercept + " \n";
+        toString = toString + "Distance between points: " + distance + " \n";
         return toString;
     }
 }
